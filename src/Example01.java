56f030858372e67d57045311e4e65b0a1ddad606
minlc <minlc@317hu.com>
2017-08-24 20:21:46 +0800

    添加Example01类的注释

--- src/Example01.java
+++ src/Example01.java
@@ -5,14 +5,15 @@ import java.io.FileInputStream;
  */
 public class Example01 {
     public static void main(String[] args) throws Exception {
+        //创建一个文件字节输入流
         FileInputStream in = new FileInputStream("test.txt");
-        int b = 0;
+        int b = 0;              //定义一个int类型的变量b，记住每次读取的一个字节
         while (true){
-            b = in.read();
-            if(b==-1){
+            b = in.read();      //变量b记住读取的一个字节
+            if(b==-1){          //如果读取的字节为-1，跳出while循环
                 break;
             }
-            System.out.println(b);
+            System.out.println(b);      //否则将b输出
         }
         in.close();
 

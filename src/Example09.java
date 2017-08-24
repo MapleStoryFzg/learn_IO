906938818ffea4a69062d63dca615c78cb12daff
minlc <minlc@317hu.com>
2017-08-24 11:14:39 +0800

    测试github提交代码

--- src/Example09.java
+++ src/Example09.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Example09 {
     public static void main(String[] args) throws IOException {
         FileWriter writer = new FileWriter("writer.txt",true);  //若要在文件末尾追加数据，则需要加一个参数true
-        String str = "你好，gtx！";
+        String str = "你好，gtx测试！";
         writer.write(str);
         writer.write("\r\n");//将输出语句换行
         writer.close();
